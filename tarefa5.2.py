a99f8720c7246d53d4dc90505d9521a503dd88f7
yulianyadevochka <yulianya.devochka@gmail.com>
2025-02-24 16:49:36 +0100

    Add files via upload

---
+++ tarefa5.2.py
@@ -0,0 +1,6 @@
+print("*********Cambio del euros a grivnas*************")
+resposta = input("Cantos euros quieres cambiar por grivnas?")
+respostaNum = int(resposta)
+grivnas = 43*respostaNum
+print("Esto son en grivnas :", grivnas)
+print("**********************")
