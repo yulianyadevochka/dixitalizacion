ff3b974434699e60c4696bf867cf6ac9a9ddb304
yulianyadevochka <yulianya.devochka@gmail.com>
2025-02-24 16:49:46 +0100

    Add files via upload

---
+++ tarefa5.3.py
@@ -0,0 +1,6 @@
+print("*********Cambio del euros a dolares*************")
+resposta = input("Cantos euros quieres cambiar por dolares?")
+respostaNum = int(resposta)
+dolares = 1.05*respostaNum
+print("Esto son en dolares :", dolares)
+print("**********************")
