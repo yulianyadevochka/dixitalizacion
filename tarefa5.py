54f4b8d9481429e8f7b1a36460201dc95fe5f559
yulianyadevochka <yulianya.devochka@gmail.com>
2025-02-24 16:49:13 +0100

    Add files via upload

---
+++ tarefa5.py
@@ -0,0 +1,6 @@
+print("*********Compras en internet*************")
+resposta = input("Cantos ducias de uvas queres comprar?")
+respostaNum = int(resposta)
+ovos = 12*respostaNum
+print("Tes que comprar este numero de ovos:", ovos)
+print("**********************")
