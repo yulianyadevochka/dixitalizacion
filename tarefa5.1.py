755e4aae621ef2ab41a8302ab4487eb8bf1ea059
yulianyadevochka <yulianya.devochka@gmail.com>
2025-02-24 16:49:27 +0100

    Add files via upload

---
+++ tarefa5.1.py
@@ -0,0 +1,6 @@
+print("*********Cuentos tuyos*************")
+resposta = input("Cantos kilometros vas de paseo?")
+respostaNum = int(resposta)
+kilometros = 1000*respostaNum
+print("Eso son", kilometros, " metros")
+print("**********************")
