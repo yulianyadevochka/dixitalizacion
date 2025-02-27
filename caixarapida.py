ae19ccbf65ab192c6831a6591e10738d1ecfab88
yulianyadevochka <yulianya.devochka@gmail.com>
2025-02-27 13:43:31 +0100

    Add files via upload

---
+++ caixarapida.py
@@ -0,0 +1,9 @@
+print("********Caixa rapida*********")
+resposta = input("Primero precio: ")
+resposta1 = int(resposta)
+respost = input("Segundo precio: ")
+resposta2 = int(respost)
+respos = input("Terceiro precio: ")
+resposta3 = int(respos)
+
+print("En total tienes que pagar:", resposta1+resposta2+resposta3, "euros")
