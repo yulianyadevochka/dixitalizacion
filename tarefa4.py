abf077c184a9f9223794343639522cfccd63151a
yulianyadevochka <yulianya.devochka@gmail.com>
2025-03-10 16:42:08 +0100

    Add files via upload

---
+++ tarefa4.py
@@ -0,0 +1,6 @@
+resp = input("Como te chamas? ");
+print("Guau... que nome máis interesante" , resp);
+print("Ti es unha grande persoa ", resp);
+print("Vales moito", resp);
+print("Non hai ninguen máis elegante ca", resp);
+print("e....",  resp, ", o mundo adórate!")
