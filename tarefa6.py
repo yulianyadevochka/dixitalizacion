34fdc78cd961d2c4dde008eb0e6da1d47a9a8fe3
yulianyadevochka <yulianya.devochka@gmail.com>
2025-03-10 16:47:46 +0100

    Add files via upload

---
+++ tarefa6.py
@@ -0,0 +1,13 @@
+persoa = input("Cal e o teu nome? ")
+if(persoa == "profe"):
+    print("Bos dias ", persoa)
+elif (persoa == "Marcos"):
+    print("Traballa en chisco")
+elif (persoa == "Pedro"):
+    print("Mira a la ventana")
+elif (persoa == "Xoel"):
+    print("No tardes a clase")
+elif (persoa == "Pita"):
+    print("Mira a Google")
+else:
+    print("Npn sei quen es")
