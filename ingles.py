38e1e9e81000f9771d9b478a98c1c470637239a3
yulianyadevochka <yulianya.devochka@gmail.com>
2025-03-11 13:58:58 +0100

    Add files via upload

---
+++ ingles.py
@@ -0,0 +1,26 @@
+print("Aprendemos palabras en ingles")
+respuesta = input("Como se dice en ingles casa? ")
+if(respuesta == "house"):
+    print("Muy bien!! Seguimos")
+else:
+    print("No es correcto:(")
+respuesta2 = input("Como se dice en ingles cielo? ")
+if(respuesta2 == "sky"):
+    print("Muy bien!! Seguimos")
+else:
+    print("No es correcto:(")
+respuesta3 = input("Como se dice en ingles oso? ")
+if(respuesta3 == "bear"):
+    print("Muy bien!! Seguimos")
+else:
+    print("No es correcto:(")
+respuesta4 = input("Como se dice en ingles sol? ")
+if(respuesta4 == "sun"):
+    print("Muy bien!! Seguimos")
+else:
+    print("No es correcto:(")
+respuesta5 = input("Como se dice en ingles flor? ")
+if(respuesta5 == "flower"):
+    print("Muy bien!! Ganaste!!")
+else:
+    print("No es correcto, intenta de nuevo en proxima vez.")
