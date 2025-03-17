215bff03967fecee1c23627f453356b0e6e910a7
yulianyadevochka <yulianya.devochka@gmail.com>
2025-03-17 16:47:58 +0100

    Add files via upload

---
+++ axencia_matrimonial.py
@@ -0,0 +1,28 @@
+print("Tu pareja ideal")
+print("Escribe solo la verdad")
+ojos = input("Cual color de ojos te gustan?")
+if(ojos == "azules"):
+    altura = input("Qué altura te gusta?")
+    if(altura == "185cm"):
+        país = input("De qual pais prefieres un novio?")
+        if(país == "Reino Unido"):
+            print("Tuya pareja ideal es Robert Pattinson")
+            print("Felicidades!!!")
+elif(ojos == "marrones"):
+    altura = input("Qué altura te gusta?")
+    if(altura == "183cm"):
+        país = input("De qual pais prefieres un novio?")
+        if(país == "Inglatera"):
+            print("Tuya pareja ideal es David Beckham")
+            print("Felicidades!!!")
+elif(ojos == "azules"):
+    altura = input("Qué altura te gusta?")
+    if(altura == "182cm"):
+        país = input("De qual pais prefieres un novio?")
+        if(país == "USA"):
+            print("Tuya pareja ideal es Adam Levine")
+            print("Felicidades!!!")
+             
+              
+    
+
